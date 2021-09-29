f604fd1cf49962cf05308a81f7bfce7c8eda1f64
shuang zheng <shuangzheng16@gmail.com>
2021-09-29 14:32:24 -0700

    user class

--- W5/D3/test.rb
+++ W5/D3/test.rb
@@ -2,7 +2,7 @@ require 'sqlite3'
 require 'singleton'
 
 class QuestionsDatabase < sqlite3::Database
-    include singleton
+    include Singleton
 
     def initialize
         super('questions.db')
@@ -10,3 +10,43 @@ class QuestionsDatabase < sqlite3::Database
         self.results_as_hash = true
     end
 end
+
+class Users
+
+    def self.all
+        data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+        data.map { |datum| Users.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        user_id = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                id = ?
+        SQL
+        return nil unless user_id.length > 0
+        user_id.map{|ele| Users.new(ele)}
+
+    end
+
+    def self.find_by_name(fname, lname)
+        #raise error
+        user = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                fname = ? AND lname = ?
+        SQL
+        return nil unless user.length > 0
+        user.map{|ele| Users.new(ele)}
+    end
+
+end
+
+
+
