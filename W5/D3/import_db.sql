24269fa6266ceb7a166020cd34f6ecea5d094376
Alan Chiu <ms0372631@gmail.com>
2021-09-29 11:23:38 -0700

    working on INSERT INTO, VALUE

--- W5/D3/import_db.sql
+++ W5/D3/import_db.sql
@@ -1,3 +1,6 @@
+
+PRAGMA foreign_keys = ON;
+
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -5,6 +8,7 @@ CREATE TABLE users (
   fname TEXT NOT NULL,
   lname TEXT NOT NULL
 );
+
 
 DROP TABLE IF EXISTS questions;
 
@@ -25,7 +29,7 @@ CREATE TABLE question_follows (
   question_id INTEGER NOT NULL, -- can be null?
   author_id INTEGER NOT NULL,
 
-  FOREIGN KEY(author_id) REFERENCES users(user_id)
+  FOREIGN KEY(author_id) REFERENCES users(user_id),
   FOREIGN KEY(question_id) REFERENCES questions(question_id)
 );
 
@@ -38,7 +42,43 @@ CREATE TABLE replies (
   author_id INTEGER,
   body TEXT NOT NULL,
 
+  FOREIGN KEY(author_id) REFERENCES users(user_id),
+  FOREIGN KEY(question_id) REFERENCES questions(question_id)
+
 );
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+  total_likes INTEGER DEFAULT 0,
+  author_id INTEGER NOT NULL,
+  question_id INTEGER NOt NULL,
+
+  FOREIGN KEY(author_id) REFERENCES users(user_id),
+  FOREIGN KEY(question_id) REFERENCES questions(question_id)
+
+);
+
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Edward', 'James'),
+  ('David', 'Green');
+
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('How do I open SQL file?', 'How do I open SQL file?', (SELECT user_id FROM users WHERE fname = 'Edward' AND lname = 'James')),
+  ('How to pass Ruby 2 assessment?', 'How to pass Ruby 2 assessment?', (SELECT user_id FROM users WHERE fname = 'Edward' AND lname = 'James')),
+  ('What is the weather today?', 'What is the weather today?', (SELECT user_id FROM users WHERE fname = 'David' AND lname = 'Green'));
+
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('How do I open SQL file?', 'How do I open SQL file?', 1),
+  ('How to pass Ruby 2 assessment?', 'How to pass Ruby 2 assessment?', 1),
+  ('What is the weather today?', 'What is the weather today?', 2);
 
 
 
