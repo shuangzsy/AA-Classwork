77a7ac703308a34bdd385a4a28f08bd2456a7837
shuang zheng <shuangzheng16@gmail.com>
2021-09-30 15:33:11 -0700

    user/course updates

--- W5/D4/skeleton/app/models/course.rb
+++ W5/D4/skeleton/app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ApplicationRecord
+  has_many :enrolled_students,
+  primary_key: :id,
+  foreign_key: :course_id,
+  class_name: :User
 end
--- W5/D4/skeleton/app/models/enrollment.rb
+++ W5/D4/skeleton/app/models/enrollment.rb
@@ -1,2 +1,6 @@
 class Enrollment < ApplicationRecord
+  belongs_to :course_enrolled,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course
 end
