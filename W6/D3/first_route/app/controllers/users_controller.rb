92a2ee07b8844e8ccacddcb6ff3d32f5c79eac88
Ry Mercado <rytmercado@gmail.com>
2021-10-06 16:36:27 -0600

    users controller in progress

--- W6/D3/first_route/app/controllers/users_controller.rb
+++ W6/D3/first_route/app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
+# class UsersController < ApplicationController < ActionController::Base
 class UsersController < ApplicationController
 
     def index
-        users = User.all
+        # users = User.all
+        render plain: "I'm in the index action!"
     end
+
+    def create
+        render json: params
+    end
+
+
 
 
 end
