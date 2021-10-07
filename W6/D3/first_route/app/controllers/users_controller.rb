3c27cc29fc4cffe90cb59886b603885250733c33
shuang zheng <shuangzheng16@gmail.com>
2021-10-06 17:25:29 -0700

    controllers updated

--- W6/D3/first_route/app/controllers/users_controller.rb
+++ W6/D3/first_route/app/controllers/users_controller.rb
@@ -2,12 +2,28 @@
 class UsersController < ApplicationController
 
     def index
-        # users = User.all
-        render plain: "I'm in the index action!"
+        users = User.all
+        render json: users
     end
 
     def create
-        render json: params
+        user = User.new(
+            params.require(:users).permit(:name, :email)
+        )
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user
+        else
+            render json: { error: "there is no user with that ID #{params[:id]}"}, status: 404
+        end
     end
 
 
---
+++ W6/D3/first_route/db/migrate/20211006231842_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.timestamps
+    end
+    add_index :users, :name
+    add_index :users, :email, unique: true
+  end
+end
