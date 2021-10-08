86f3573ac4ce84b52de1ff2e82f85cb97317c19e
wesley <cheung.w.2016@gmail.com>
2021-10-08 15:17:35 -0700

    added age method in Cat class

--- W6/D5/cats_99/app/models/cat.rb
+++ W6/D5/cats_99/app/models/cat.rb
@@ -1,2 +1,13 @@
+require 'action_view'
 class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+  validates :birth_date, presence: true
+  def age 
+    time_ago_in_words(birth_date)
+    # now = Date.parse(Time.now.utc).year
+    # birthday = Date.parse(self.birth_date).year
+    
+    # return now - birthday
+  end
+
 end
