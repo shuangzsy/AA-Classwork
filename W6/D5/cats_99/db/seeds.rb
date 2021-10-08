2b7828b62693941cdc87549c9fb34997529c887c
shuang zheng <shuangzheng16@gmail.com>
2021-10-08 16:56:12 -0700

    show/index

--- W6/D5/cats_99/db/seeds.rb
+++ W6/D5/cats_99/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cat_1 = Cat.new(birth_date: '2015/01/20', color: 'black', name: 'grinch', sex: 'M', description: 'I am a cat.')
+cat_2 = Cat.new(birth_date: '2017/01/20', color: 'white', name: 'ophelia', sex: 'F', description: 'I am beautiful.')
+cat_3 = Cat.new(birth_date: '2020/01/20', color: 'gray', name: 'levi', sex: 'M', description: 'I like Jeans.')
