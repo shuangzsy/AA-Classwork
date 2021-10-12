928bd76076c5872d89bda1223cfaf34165656bb8
shuang zheng <shuangzheng16@gmail.com>
2021-10-11 17:28:40 -0700

    controllers

--- W7/D1/skeleton/app/controllers/application_controller.rb
+++ W7/D1/skeleton/app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logout
+
+  end
+
 end
--- W7/D1/skeleton/app/controllers/sessions_controller.rb
+++ W7/D1/skeleton/app/controllers/sessions_controller.rb
@@ -4,9 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
+    if @user
+      redirect_to cats_url
+    else
+      render json: ["invalid username or password"]
+    end
   end
 
   def destroy
+    
+    redirect_to new_session_url
   end
 end
