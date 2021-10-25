4051a143fb784f23565c73613b482de2273c89f7
shuang zheng <shuangzheng16@gmail.com>
2021-10-25 11:03:07 -0700

    initilize movingObject

---
+++ W9/D1/src/index.js
@@ -0,0 +1,5 @@
+console.log("Webpack is working!")
+
+const MovingObject = require("./moving_object.js");
+
+window.MovingObject = MovingObject;
---
+++ W9/D1/src/moving_object.js
@@ -0,0 +1,13 @@
+function MovingObject(options){
+  this.pos = options.pos;
+  this.vel = options.vel;
+  this.radius = options.radius;
+  this.color = options.color;
+}
+
+MovingObect.prototype.draw = function(ctx){
+  
+}
+
+module.exports = MovingObject;
+
