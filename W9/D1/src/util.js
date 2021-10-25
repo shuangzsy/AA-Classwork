4a6a561b979d7e607ac7943d31eda3e31f4f1cfe
David Chan <dchaan1@gmail.com>
2021-10-25 15:12:55 -0700

    Made changes

--- W9/D1/src/util.js
+++ W9/D1/src/util.js
@@ -4,6 +4,15 @@ const Util = {
     Surrogate.prototype = parentClass.prototype;
     childClass.prototype = new Surrogate;
     childClass.prototype.contructor = childClass;
+  },
+
+  randomVec(length) {
+    const deg = 2 * Math.PI * Math.random();
+    return Util.scale([Math.sin(deg), Math.cos(deg)], length);
+  },
+  // Scale the length of a vector by the given amount.
+  scale(vec, m) {
+    return [vec[0] * m, vec[1] * m];
   }
 };
 
